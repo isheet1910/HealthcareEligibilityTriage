ee8ef2c4325c2a0d8c0924390b33ffe08740cc3f
ISHEET SHETTY <isheetshetty@gmail.com>
2026-06-18 12:46:56 -0700

    Final run

--- src/run_all.py
+++ src/run_all.py
@@ -28,6 +28,7 @@ NORMALIZE_SCRIPT  = os.path.join(BASE, "normalize.py")
 RULES_SCRIPT      = os.path.join(BASE, "rules.py")
 STREAMLIT_APP     = os.path.join(BASE, "app.py")
 STREAMLIT_BIN = os.path.join(BASE, "..", "venv", "Scripts", "streamlit.exe")
+PYTHON = sys.executable
 
 
 
@@ -59,7 +60,8 @@ def main():
     # 4. Launch Streamlit UI
     print("\n===== STEP: Launching Streamlit App =====")
     print("Opening dashboard...\n")
-    subprocess.run([STREAMLIT_BIN, "run", STREAMLIT_APP])
+    # subprocess.run([STREAMLIT_BIN, "run", STREAMLIT_APP])
+    subprocess.run([PYTHON, "-m", "streamlit", "run", STREAMLIT_APP])
 
 
 if __name__ == "__main__":
